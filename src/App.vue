8c6dae457c6b21ae0276f15863db336f59b3d510
JIU141 <1219932710@qq.com>
2023-11-06 19:18:27 +0800

    合并

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    asdas
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
