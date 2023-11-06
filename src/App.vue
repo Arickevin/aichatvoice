c4e318d161d495da285824b7054e06227659c14d
Arickevin <3307765651@qq.com>
2023-11-06 19:10:31 +0800

    没改什么

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@ import HelloWorld from './components/HelloWorld.vue'
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
+    </a>   
+     <!-- fjodisajfodasdi -->
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
